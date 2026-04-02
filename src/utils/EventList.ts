5969136f724285f4eb6baa90d868858c4dbad570
NicholasKeenan <nicholas.keenan@maine.edu>
2026-04-02 14:58:33 -0400

    Basic EventList.ts framework

--- src/utils/EventList.ts
+++ src/utils/EventList.ts
@@ -1,6 +1,8 @@
 //File containing the functions to manage the different events
 //within the application. 
 
+import { EventEntry } from "./EventEntry";
+
 //EventList object to store the different events and their corresponding functions
 
 //events:
@@ -11,3 +13,28 @@
 //removeDate ( int index )
 
 //exportAsICS()
+
+export class EventList {
+
+    private events: Array<EventEntry>;
+
+    constructor() {
+        this.events = [];
+    }
+
+    //function to add an event to the list
+    addData ( event: EventEntry ) {
+        return 0;
+    }
+
+    //function to remove an event from the list
+    removeData ( index: number ) {
+        return 0;
+    }
+
+    //function to export the events as an ICS file
+    exportAsICS() { //function to export the events as an ICS file
+        return 0;
+    }
+
+}
