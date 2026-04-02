478745f0071ade3abbbe0574ffe7a10ea62a39f8
NicholasKeenan <nicholas.keenan@maine.edu>
2026-04-02 14:32:35 -0400

    Create EventList.ts

---
+++ src/utils/EventList.ts
@@ -0,0 +1,13 @@
+//File containing the functions to manage the different events
+//within the application. 
+
+//EventList object to store the different events and their corresponding functions
+
+//events:
+//List<EventEntry>
+
+//addDate()
+
+//removeDate ( int index )
+
+//exportAsICS()
