d3b161f48726438a384ecbe4f3a7a79d9542a148
NicholasKeenan <nicholas.keenan@maine.edu>
2026-04-09 21:28:02 -0400

    Fixed DownloadPage test

--- src/DownloadPage.test.tsx
+++ src/DownloadPage.test.tsx
@@ -1,20 +1,43 @@
 import { render, screen } from "@testing-library/react";
 import { DownloadPage } from "./DownloadPage";
+import { MemoryRouter } from "react-router";
+import { EventList } from "./utils/EventList";
+
+function renderWithRouter() {
+  const fakeEventList = new EventList( [] );
+  fakeEventList.events = []; // or leave default if constructor sets it
+
+  return render(
+    <MemoryRouter
+      initialEntries={[
+        {
+          pathname: "/",
+          state: { result: fakeEventList }
+        } as any
+      ]}
+    >
+      <DownloadPage />
+    </MemoryRouter>
+  );
+}
 
 test('renders a download button', () => {
-  render(<DownloadPage/>);
+  renderWithRouter();
   const button = screen.getByRole('button', { name: /Download ics file/i });
   expect(button).toBeInTheDocument();
+
 });
 
 test('renders add event button', () => {
-  render(<DownloadPage/>);
+  renderWithRouter();
   const button = screen.getByRole('button', { name: /Add Event/i});
   expect(button).toBeInTheDocument();
 });
 
 test('renders edit event button', () => {
-  render(<DownloadPage/>);
+
+  renderWithRouter();
+  
   const button = screen.getByRole('button', { name: /Edit Event/i});
   expect(button).toBeInTheDocument();
 });
